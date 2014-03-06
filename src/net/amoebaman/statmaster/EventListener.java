80a9da9df183172389733a7866f71e69531ba747
Dennison Richter <drichter@amoebaman.net>
2014-03-06 05:54:32 -0500

    Implement some basic statistics

--- src/net/amoebaman/statmaster/EventListener.java
+++ src/net/amoebaman/statmaster/EventListener.java
@@ -4,16 +4,29 @@ import net.amoebaman.statmaster.events.KillingSpreeEvent;
 import net.amoebaman.statmaster.events.MultiKillEvent;
 import net.amoebaman.utils.ChatUtils;
 import net.amoebaman.utils.ChatUtils.ColorScheme;
+import net.amoebaman.utils.GenUtil;
 import net.amoebaman.utils.maps.PlayerMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType.SlotType;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.event.player.PlayerFishEvent.State;
 
 public class EventListener implements Listener {
 	
@@ -35,57 +48,111 @@ public class EventListener implements Listener {
 		}}, 0, 20);
 	}
 	
-	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent event){
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void playerKillStatistics(PlayerDeathEvent event){
 		final Player victim = event.getEntity();
 		if(victim == null)
 			return;
-		Player tempKiller = victim.getKiller();
-		if(tempKiller == null){
-			try{ tempKiller = (Player) ((Wolf)((EntityDamageByEntityEvent) victim.getLastDamageCause()).getDamager()).getOwner(); }
-			catch(ClassCastException cce){}
-			catch(NullPointerException npe){}
+		if(victim.getLastDamageCause() instanceof EntityDamageByEntityEvent){
+			LivingEntity culprit = GenUtil.getTrueCulprit((EntityDamageByEntityEvent) victim.getLastDamageCause());
+			if(culprit instanceof Player){
+				final Player killer = (Player) culprit;
+				
+				StatMaster.getHandler().incrementStat(killer, "player kills");
+				StatMaster.getHandler().incrementStat(victim, "player deaths");
+				StatMaster.getHandler().incrementCommunityStat("player kills");
+				StatMaster.getHandler().updateStat(killer, "ktd", StatMaster.getHandler().getStat(killer, "player kills") / StatMaster.getHandler().getStat(killer, "player deaths"));
+				StatMaster.getHandler().updateStat(victim, "ktd", StatMaster.getHandler().getStat(victim, "player kills") / StatMaster.getHandler().getStat(victim, "player deaths"));
+				
+				double eloChange = (double) (1.0 * StatMaster.getHandler().getStat(victim, "elo") / StatMaster.getHandler().getStat(killer, "elo") * 15);
+				StatMaster.getHandler().adjustStat(victim, "elo_skill", -eloChange);
+				StatMaster.getHandler().adjustStat(killer, "elo_skill", eloChange);
+				StatMaster.getHandler().adjustCommunityStat("elo_change", eloChange);
+				
+				Bukkit.getScheduler().scheduleSyncDelayedTask(StatMaster.getPlugin(StatMaster.class), new Runnable(){ public void run(){
+					
+					killSpree.put(killer, killSpree.get(killer) + 1);
+					multiKill.put(killer, multiKill.get(killer) + 1);
+					lastKill.put(killer, System.currentTimeMillis());
+					
+					if(killSpree.get(killer) >= 3){
+						Bukkit.broadcastMessage(ChatUtils.format(" --> " + killer.getName() + " is on a spree of [[" + killSpree.get(killer) + "]] kills", ColorScheme.NORMAL));
+						if(killSpree.get(killer) > StatMaster.getHandler().getStat(killer, "longest spree"))
+							StatMaster.getHandler().updateStat(killer, "longest spree", killSpree.get(killer));
+						new KillingSpreeEvent(killer, killSpree.get(killer), false).callEvent();
+					}
+					
+					if(killSpree.get(victim) >= 3){
+						Bukkit.broadcastMessage(ChatUtils.format(" --> Ended [[" + victim.getName() + "]]'s spree at [[" + killSpree.get(victim) + "]] kills", ColorScheme.NORMAL));
+						new KillingSpreeEvent(victim, killSpree.get(victim), true).callEvent();
+					}
+					killSpree.put(victim, 0);
+					
+				}});
+			}
+			else if(culprit != null)
+				StatMaster.getHandler().incrementStat(victim, "monster deaths");
 		}
-		if(tempKiller == null && victim.getLastDamageCause() instanceof EntityDamageByEntityEvent)
-			if(((EntityDamageByEntityEvent) victim.getLastDamageCause()).getCause() == DamageCause.CUSTOM){
-				try{ tempKiller = (Player) ((EntityDamageByEntityEvent) victim.getLastDamageCause()).getDamager(); }
-				catch(ClassCastException e){}
-			}
-		if(victim == null || tempKiller == null)
-			return;
-		final Player killer = tempKiller;
-		
-		StatMaster.getHandler().incrementStat(killer, "kills");
-		StatMaster.getHandler().incrementStat(victim, "deaths");
-		StatMaster.getHandler().incrementCommunityStat("kills");
-		StatMaster.getHandler().updateStat(killer, "ktd", StatMaster.getHandler().getStat(killer, "kills") / StatMaster.getHandler().getStat(killer, "deaths"));
-		StatMaster.getHandler().updateStat(victim, "ktd", StatMaster.getHandler().getStat(victim, "kills") / StatMaster.getHandler().getStat(victim, "deaths"));
-		
-		double eloChange = (double) (1.0 * StatMaster.getHandler().getStat(victim, "elo") / StatMaster.getHandler().getStat(killer, "elo") * 15);
-		StatMaster.getHandler().adjustStat(victim, "elo_skill", -eloChange);
-		StatMaster.getHandler().adjustStat(killer, "elo_skill", eloChange);
-		StatMaster.getHandler().adjustCommunityStat("elo_change", eloChange);
-		
-		Bukkit.getScheduler().scheduleSyncDelayedTask(StatMaster.getPlugin(StatMaster.class), new Runnable(){ public void run(){
-			
-			killSpree.put(killer, killSpree.get(killer) + 1);
-			multiKill.put(killer, multiKill.get(killer) + 1);
-			lastKill.put(killer, System.currentTimeMillis());
-			
-			if(killSpree.get(killer) >= 3){
-				Bukkit.broadcastMessage(ChatUtils.format(" --> " + killer.getName() + " is on a spree of [[" + killSpree.get(killer) + "]] kills", ColorScheme.NORMAL));
-				if(killSpree.get(killer) > StatMaster.getHandler().getStat(killer, "longest spree"))
-					StatMaster.getHandler().updateStat(killer, "longest spree", killSpree.get(killer));
-				new KillingSpreeEvent(killer, killSpree.get(killer), false).callEvent();
-			}
-			
-			if(killSpree.get(victim) >= 3){
-				Bukkit.broadcastMessage(ChatUtils.format(" --> Ended [[" + victim.getName() + "]]'s spree at [[" + killSpree.get(victim) + "]] kills", ColorScheme.NORMAL));
-				new KillingSpreeEvent(victim, killSpree.get(victim), true).callEvent();
-			}
-			killSpree.put(victim, 0);
-			
-		}});
+		else
+			StatMaster.getHandler().incrementStat(victim, "natural deaths");
 	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void mobKillStatistics(EntityDeathEvent event){
+		LivingEntity victim = event.getEntity();
+		if(victim == null || victim instanceof Player)
+			return;
+		if(victim.getLastDamageCause() instanceof EntityDamageByEntityEvent){
+			LivingEntity culprit = GenUtil.getTrueCulprit((EntityDamageByEntityEvent) victim.getLastDamageCause());
+			if(culprit instanceof Player){
+				if(victim instanceof Animals)
+					StatMaster.getHandler().incrementStat((Player) culprit, "animal kills");
+				if(victim instanceof Monster)
+					StatMaster.getHandler().incrementStat((Player) culprit, "monster kills");
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void blockBreakStatistics(BlockBreakEvent event){
+		Player player = event.getPlayer();
+		Material tool = player.getItemInHand().getType();
+		if(tool.name().contains("_PICKAXE"))
+			StatMaster.getHandler().incrementStat(player, "blocks mined");
+		else if(tool.name().contains("_AXE"))
+			StatMaster.getHandler().incrementStat(player, "blocks mined");
+		else if(tool.name().contains("_SPADE"))
+			StatMaster.getHandler().incrementStat(player, "blocks dug");
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void pyromaniacStatistic(BlockIgniteEvent event){
+		if(event.getCause() == IgniteCause.FLINT_AND_STEEL && event.getIgnitingEntity() != null)
+			StatMaster.getHandler().incrementStat((Player) event.getIgnitingEntity(), "blocks torched");
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void fishingStatistic(PlayerFishEvent event){
+		if(event.getState() == State.CAUGHT_FISH)
+			StatMaster.getHandler().incrementStat(event.getPlayer(), "fish caught");
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void craftingStatistics(InventoryClickEvent event){
+		if(event.getSlotType() == SlotType.RESULT){
+			if(event.getCurrentItem().getType() == Material.CAKE)
+				StatMaster.getHandler().incrementStat((Player) event.getWhoClicked(), "cakes baked");
+			if(event.getCurrentItem().getType() == Material.GOLDEN_APPLE && event.getCurrentItem().getDurability() == 1)
+				StatMaster.getHandler().adjustStat((Player) event.getWhoClicked(), "notch apples crafted", 1);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void foodStatistic(FoodLevelChangeEvent event){
+		Player player = (Player) event.getEntity();
+		if(event.getFoodLevel() > player.getFoodLevel())
+			StatMaster.getHandler().adjustStat(player, "food eaten", event.getFoodLevel() - player.getFoodLevel());
+	}
+
 	
 }
